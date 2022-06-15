1b174a45755a792ed51603a40c47638b53b209eb
kwang-sub <chlrhkdtjq11@naver.com>
2022-06-15 11:42:05 +0900

    제어문 기초 완료 자료구조 시작

--- src/book/question/chap01/Q04.java
+++ src/book/question/chap01/Q04.java
@@ -1,19 +1,12 @@
 package book.question.chap01;
 
+import java.util.Scanner;
+
 public class Q04 {
     public static void main(String[] args) {
-        gugudan();
-        System.out.print("   |");
-        for (int i = 1; i <= 9; i++)
-            System.out.printf("%3d", i);
-        System.out.println("\n---+---------------------------");
-
-        for (int i = 1; i <= 9; i++) {
-            System.out.printf("%2d |", i);
-            for (int j = 1; j <= 9; j++)
-                System.out.printf("%3d", i * j);
-            System.out.println();
-        }
+//        gugudan();
+//        plus();
+        square();
     }
 /*내가 그린 구구단....모든걸 하나의 반복문 안에서 해결해야된다고 생각했네...해답보면 for문 필요한 부분 따로따로 돌렸음..
 그리고 %d사용시 %3d사용하면 공간 확보... 띄어쓰기로 간격 맞출필요 없었음...*/
@@ -45,6 +38,53 @@ public class Q04 {
                         System.out.print("---");
                     }
                 }
+            }
+            System.out.println();
+        }
+
+//정답지에서 제공한 방식
+        System.out.print("   |");
+        for (int i = 1; i <= 9; i++)
+            System.out.printf("%3d", i);
+        System.out.println("\n---+---------------------------");
+
+        for (int i = 1; i <= 9; i++) {
+            System.out.printf("%2d |", i);
+            for (int j = 1; j <= 9; j++)
+                System.out.printf("%3d", i * j);
+            System.out.println();
+        }
+    }
+
+//    위에서 했던 구구단과 같은 형식으로 덧셈을 표현하세요
+    static void plus() {
+        System.out.print("  |");
+        for(int i = 1; i < 10; i++) {
+            System.out.printf("%3d", i);
+        }
+        System.out.println("\n--+---------------------------");
+
+        for (int i = 1; i < 10; i++) {
+            for (int j = 1; j < 10; j++) {
+                if (j == 1) {
+                    System.out.printf(" %d|%3d", i, i + j);
+                }else {
+                    System.out.printf("%3d", i + j);
+                }
+            }
+            System.out.println();
+        }
+    }
+
+//    수를 입력 받고 그 수에 맞는 정사각형을 *로 출력하세요
+    static void square() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("수를 입력하세요");
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0;  j < n; j++) {
+                System.out.print("*");
             }
             System.out.println();
         }
